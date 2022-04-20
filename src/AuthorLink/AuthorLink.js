ad84984cf87a19b4e96326f3bbac51cc5a2d63c0
Moshe <shootermv@gmail.com>
2022-04-20 19:48:36 +0300

    refactor: authorLink moved to its directory

--- src/AuthorLink/AuthorLink.js
+++ src/AuthorLink/AuthorLink.js
@@ -1,10 +1,8 @@
+import "./AuthorLink.css";
 export default function AuthorLink({ who }) {
   return (
     <a href={`https://github.com/${who.login}`} target="_blank">
-      <img
-        src={who.avatar_url}
-        style={{ borderRadius: "50%", height: "22px" }}
-      />
+      <img src={who.avatar_url} className="author-link" />
     </a>
   );
 }
