80b35113031939d965d91151200f838c23cd17b4
Moshe <shootermv@gmail.com>
2022-04-24 12:49:07 +0300

    refactor: rename primary to primary-color

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,6 +6,8 @@ import {
   faSun*/
 } from "@fortawesome/free-regular-svg-icons";
 //import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import "./Header.css";
 export default function Header() {
   return (
     <header>
