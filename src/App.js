0b56467142f2a6a259ea2ef48a62590f5cabfd4a
Moshe <shootermv@gmail.com>
2022-04-24 07:52:36 +0300

    refactor: move header to its own folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 import { Routes, Route, Link } from "react-router-dom";
 import { Commits } from "./Pages/Commits";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
-      <header>REACT NEWS</header>
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Commits />} />
