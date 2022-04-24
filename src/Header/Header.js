c445dd30e55defc0d70bb965aacc76a29ed7d841
Moshe <shootermv@gmail.com>
2022-04-24 10:14:59 +0300

    feat: adding bell icon at header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,3 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell
+  /*faListAlt,
+  faMoon,
+  faSun*/
+} from "@fortawesome/free-regular-svg-icons";
+//import { faBars } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
-  return <header>REACT NEWS</header>;
+  return (
+    <header>
+      <FontAwesomeIcon icon={faBell} />
+      REACT NEWS
+    </header>
+  );
 }
