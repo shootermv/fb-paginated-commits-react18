ef85f8e202787c042a2622822077fde8948b3e4e
Moshe <shootermv@gmail.com>
2022-04-28 18:13:28 +0300

    refactor: move header component to Layout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Suspense, lazy, useState } from "react";
 import Loader from "./Loader";
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./Header";
-import Footer from "./Layout/Footer/Footer";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
 import SideNav from "./SideNav";
 import { Commits as CommitsPage } from "./Pages/Commits";
 const PrsPage = lazy(() => import("./Pages/Prs"));
