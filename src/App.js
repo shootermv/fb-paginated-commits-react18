b6107836ca4b9f708437984a3f091e3c030e301c
Moshe <shootermv@gmail.com>
2022-04-29 09:35:56 +0300

    feat: adding layout context

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./styles.css";
 import { Suspense, lazy, useState } from "react";
 import Loader from "./Loader";
 import { Routes, Route } from "react-router-dom";
-
+import LayoutCtx from "./Contexts/LayoutCtx";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import SideNav from "./Layout/SideNav";
@@ -13,25 +13,27 @@ export default function App() {
   const [menuShown, setMenuShown] = useState(false);
   const toggleMenu = () => setMenuShown(!menuShown);
   return (
-    <div className="MainWrap">
-      <SideNav menuShown={menuShown} />
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<CommitsPage />} />
-            <Route
-              path="/prs"
-              element={
-                <Suspense fallback={<Loader />}>
-                  <PrsPage />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </main>
-        <Footer />
+    <LayoutCtx.Provider value={{ toggleMenu, menuShown }}>
+      <div className="MainWrap">
+        <SideNav menuShown={menuShown} />
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<CommitsPage />} />
+              <Route
+                path="/prs"
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <PrsPage />
+                  </Suspense>
+                }
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </LayoutCtx.Provider>
   );
 }
--- src/Layout/Header/SideNavToggle/SideNavToggle.js
+++ src/Layout/Header/SideNavToggle/SideNavToggle.js
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import LayoutCtx from "../../../Contexts/LayoutCtx";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./SideNavToggle.css";
-export default function SideNavToggle({ toggleMenu, menuShown }) {
-  //const toggleMenu = (e) => {};
+import { useContext } from "react";
+export default function SideNavToggle() {
+  const { toggleMenu, menuShown } = useContext(LayoutCtx);
   return (
     <label className="menu-switch" htmlFor="menu-checkbox">
       <input
