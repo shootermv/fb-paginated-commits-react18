fa4318d0a627420886204a67cf2bd1bd0a6aa422
Moshe <shootermv@gmail.com>
2022-05-04 12:25:39 +0300

    feat: trying to  load PRs

--- src/Pages/Prs.js
+++ src/Pages/Prs.js
@@ -1,10 +1,13 @@
-import { Suspense } from "react";
+import useFetchData from "../useFetchData";
 import Loader from "../Loader";
+import ErrorCmp from "../Error";
 
 export default function Prs() {
+  const { data: pulls, error, loading } = useFetchData("pulls");
   return (
     <div className="App">
       <h1>Pull Requests</h1>
+      {loading ? <Loader /> : error ? <ErrorCmp /> : !!pulls}
     </div>
   );
 }
