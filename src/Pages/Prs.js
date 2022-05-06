59cb40ff670d1851e9f0cb1412c0ef1b92eaa9f3
Moshe <shootermv@gmail.com>
2022-05-06 09:27:07 +0300

    chore: add mockPr json file

--- src/Pages/Prs.js
+++ src/Pages/Prs.js
@@ -3,11 +3,13 @@ import Loader from "../Loader";
 import ErrorCmp from "../Error";
 
 export default function Prs() {
-  const { data: pulls, error, loading } = useFetchData("pulls");
+  const { data: pulls, error, loading } = useFetchData("pulls", (data) =>
+    data.slice(0, 5)
+  );
   return (
     <div className="App">
       <h1>Pull Requests</h1>
-      {loading ? <Loader /> : error ? <ErrorCmp /> : !!pulls}
+      {loading ? <Loader /> : error ? <ErrorCmp /> : pulls.length}
     </div>
   );
 }
