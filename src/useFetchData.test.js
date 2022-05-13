0250ef3d03e0efed8aaa42ba0c7ac68af68a7756
Moshe Vilner <moshe.vilner@nielsen.com>
2022-05-13 08:10:13 +0300

    test: add check eror is null

--- src/useFetchData.test.js
+++ src/useFetchData.test.js
@@ -5,7 +5,9 @@ test("should return error if 'url' params not passed", () => {
   const {result} = renderHook(() => useFetchData())
   expect(result.current.error).toBe('must pass url param!');
 });
-test("should return set loading to 'true' if 'url' params passed", () => {
+test("should return loading set to 'true' if 'url' params passed", () => {
   const {result} = renderHook(() => useFetchData('zzz'))
   expect(result.current.loading).toBeTruthy();
+  expect(result.current.error).toBeNull()
+  expect(result.current.data).toEqual([])
 });
