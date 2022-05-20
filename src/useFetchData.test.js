1333926672557aa8ea05e6ef386f2b284ad53817
Moshe Vilner <moshe.vilner@nielsen.com>
2022-05-20 11:32:32 +0300

    test: take care of 'act' warning

--- src/useFetchData.test.js
+++ src/useFetchData.test.js
@@ -14,13 +14,14 @@ test("should return error if 'url' params not passed", () => {
   const {result} = renderHook(() => useFetchData())
   expect(result.current.error).toBe('must pass url param!');
 });
-test("should return loading set to 'true' if 'url' params passed", () => {
-  const {result} = renderHook(() => useFetchData('zzz'))
+test("should return loading set to 'true' if 'url' params passed", async () => {
+  const {result, waitForNextUpdate} = renderHook(() => useFetchData('zzz'))
   expect(result.current.loading).toBeTruthy();
   expect(result.current.error).toBeNull()
   expect(result.current.data).toEqual([])
+  await waitForNextUpdate()
 });
-test("should return data",async () => {
+test("should return data", async () => {
   const {result, waitForNextUpdate} = renderHook(() =>  useFetchData('zzz'))
   await waitForNextUpdate()
   expect(result.current.data).toEqual([{nm: 'vova'}])
