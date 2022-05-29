8fde214a95b957d8bc65a84026297dfbd276c0b0
Moshe Vilner <moshe.vilner@nielsen.com>
2022-05-29 12:38:09 +0300

    chore: add tsconfig file

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = createRoot(rootElement!);
 
 root.render(
   <StrictMode>
