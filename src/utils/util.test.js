f7208b70b5ca9e95243b638476e438a6e734fc4c
moshe vilner <shootermv@gmail.com>
2022-09-05 22:23:04 +0300

    fix(test): each replaced by repeat

--- src/utils/util.test.js
+++ src/utils/util.test.js
@@ -4,16 +4,16 @@ test("pass empty args", () => {
   expect(formatMessage("", "", false)).toBe("");
 });
 test("pass 10 chars long str", () => {
-  expect(formatMessage("1".each(10), "", false).length).toBe(10);
+  expect(formatMessage("1".repeat(10), "", false).length).toBe(10);
 });
 test("pass 101 chars long str & showMore=false - should display only 100", () => {
   const showMore = false;
-  let str = "1".each(101);
+  let str = "1".repeat(101);
   expect(formatMessage(str, "", showMore).length).toBe(100);
 });
 test("if showMore=true - should display all chars", () => {
   const showMore = true;
-  let str = "1".each(101);
+  let str = "1".repeat(101);
   expect(formatMessage(str, "", showMore).length).toBe(101);
 });
 test("pass filter - should wrap term with <b>", () => {
