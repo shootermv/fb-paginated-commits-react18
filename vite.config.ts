6b8776abaca7336825f58468effcfb090bb130f6
SamMrach <sammerche84@gmail.com>
2023-08-14 16:19:25 +0100

    move from cra to vite

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, transformWithEsbuild } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+
+    // Workaround
+    {
+      name: 'load+transform-js-files-as-jsx',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) {
+          return null;
+        }
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic', // 👈 this is important
+        });
+      },
+    },
+    // End workaround
+
+  ],
+
+  // Workaround before renaming .js to .jsx
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  // End workaround
+
+
+
+})
