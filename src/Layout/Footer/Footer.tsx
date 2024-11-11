c050dbb21bd4693bd4d41fde7ba2038ed7254c7d
משה וילנר <moshe_v@malam-gemel.com>
2024-11-11 14:59:51 +0200

    refactor: use tailwind utils for theme change

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import "./Footer.css";
 export default function Footer() {
   return (
-    <footer>
+    <footer className="bg-primary text-secondary">
       made by{" "}
       <a
         href="https://gs500coder.blogspot.com"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["selector", '[data-mode="dark"]'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        ...colors,
+        primary: {
+          DEFAULT: "var(--primary)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+        },
+      },
     },
   },
   plugins: [],
