1ae53201004e5b63b338eed3eb97d210d67de3ca
moshe vilner <shootermv@gmail.com>
2025-04-08 10:29:05 +0300

    chore: upgrade react to latest 19.1.0

--- src/useFetchData.test.js
+++ src/useFetchData.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, waitFor } from "@testing-library/react";
 import useFetchData from "./useFetchData";
 import { expect, test, beforeEach, afterEach, vi } from "vitest";
 beforeEach(() => {
@@ -16,14 +16,15 @@ test("should return error if 'url' params not passed", () => {
   expect(result.current.error).toBe("must pass url param!");
 });
 test("should return loading set to 'true' if 'url' params passed", async () => {
-  const { result, waitForNextUpdate } = renderHook(() => useFetchData("zzz"));
+  const { result } = renderHook(() => useFetchData("zzz"));
   expect(result.current.loading).toBeTruthy();
   expect(result.current.error).toBeNull();
   expect(result.current.data).toEqual([]);
-  await waitForNextUpdate();
+  //await waitForNextUpdate();
 });
 test("should return data", async () => {
-  const { result, waitForNextUpdate } = renderHook(() => useFetchData("zzz"));
-  await waitForNextUpdate();
-  expect(result.current.data).toEqual([{ nm: "vova" }]);
+  const { result } = renderHook(() => useFetchData("zzz"));
+
+  await waitFor(() => expect(result.current.data).toMatchObject([{ nm: "vova" }]));
+  //expect(result.current.data).toEqual([{ nm: "vova" }]);
 });
