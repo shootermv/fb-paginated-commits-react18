71f80a61d8e9f717e82bc4f2334a34da7a7e5322
moshe vilner <shootermv@gmail.com>
2025-07-09 15:46:26 +0300

    test: use fetchMocker at tests

--- src/useFetchData.test.js
+++ src/useFetchData.test.js
@@ -1,30 +1,27 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import useFetchData from "./useFetchData";
-import { expect, test, beforeEach, afterEach, vi } from "vitest";
+import { expect, test, beforeEach, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+const mockData = [{ nm: "vova" }];
 beforeEach(() => {
-  vi.spyOn(global, "fetch").mockResolvedValue({
-    json: vi.fn().mockResolvedValue([{ nm: "vova" }]),
-  });
+  fetchMocker.resetMocks();
+  fetchMocker.mockResponseOnce(JSON.stringify(mockData));
 });
 
-afterEach(() => {
-  vi.restoreAllMocks();
-});
-//import { rest } from "msw";
-test("should return error if 'url' params not passed", () => {
+test("should return error when 'url' param not passed", () => {
   const { result } = renderHook(() => useFetchData());
   expect(result.current.error).toBe("must pass url param!");
 });
-test("should return loading set to 'true' if 'url' params passed", async () => {
+
+test("should return loading set to 'true' when 'url' param passed", async () => {
   const { result } = renderHook(() => useFetchData("zzz"));
   expect(result.current.loading).toBeTruthy();
   expect(result.current.error).toBeNull();
   expect(result.current.data).toEqual([]);
-  //await waitForNextUpdate();
 });
 test("should return data", async () => {
   const { result } = renderHook(() => useFetchData("zzz"));
-
-  await waitFor(() => expect(result.current.data).toMatchObject([{ nm: "vova" }]));
-  //expect(result.current.data).toEqual([{ nm: "vova" }]);
+  await waitFor(() => expect(result.current.data).toMatchObject(mockData));
 });
