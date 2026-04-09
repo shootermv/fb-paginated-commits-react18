8a0de9032198aef5337ab68658d3085b41da10c4
moshe vilner <shootermv@gmail.com>
2026-04-09 18:09:27 +0300

    test: use vitest-fetch-mock for mocking api call

--- src/Pages/Commits.test.tsx
+++ src/Pages/Commits.test.tsx
@@ -1,23 +1,18 @@
 import { render, cleanup, waitFor, screen } from "@testing-library/react";
 import { Commits } from "./Commits";
+import { expect, test, beforeEach, afterEach } from "vitest";
 
-import { expect, test, beforeEach, afterEach, vi } from "vitest";
 beforeEach(() => {
-  vi.spyOn(global, "fetch").mockResolvedValue({
-    json: vi.fn().mockResolvedValue([]),
-  } as any);
+  fetchMock.resetMocks();
 });
 
 afterEach(() => {
-  vi.restoreAllMocks();
   cleanup();
 });
 
 test("when fetched empty array - should render 'no records found' message", async () => {
-  vi.spyOn(global, "fetch").mockResolvedValue({
-    json: vi.fn().mockResolvedValue([]),
-  } as any);
-  const { debug, queryByTestId } = render(<Commits />);
+  fetchMock.mockResponseOnce(JSON.stringify([]));
+  const { queryByTestId } = render(<Commits />);
   await waitFor(() => {
     expect(queryByTestId("no-data-label")?.textContent || "").toContain(
       "no results"
@@ -31,21 +26,17 @@ test("should render some records", async () => {
     author: { avatar_url: "uuu", login: "jjj" },
     commit: { committer: { date: "2022-04-03T01:41:49Z" }, message: "jo" },
   };
-  vi.spyOn(global, "fetch").mockResolvedValue({
-    json: vi.fn().mockResolvedValue([record]),
-  } as any);
+  fetchMock.mockResponseOnce(JSON.stringify([record]));
 
-  const { debug, getByTestId } = render(<Commits />);
-  await waitFor( () => {
+  render(<Commits />);
+  await waitFor(() => {
     expect(screen.getByTestId("data-table")).toBeTruthy();
   });
 });
 
 test("should show loader when loading and hide it after request ended", async () => {
-  vi.spyOn(global, "fetch").mockResolvedValue({
-    json: vi.fn().mockResolvedValue([]),
-  } as any);
-  const { debug, container } = render(<Commits />);
+  fetchMock.mockResponseOnce(JSON.stringify([]));
+  const { container } = render(<Commits />);
   expect(container.querySelector("svg")).toBeTruthy();
   await waitFor(() => {
     expect(container.querySelector("svg")).not.toBeTruthy();
--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,9 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
+import { afterEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import createFetchMock from "vitest-fetch-mock";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
 
 afterEach(() => {
   cleanup();
